7f4cc0ce47ebf90e899b86487e6ea239bdee9709
Lautaro Decima <decimalautaro@gmail.com>
2024-07-04 21:04:09 -0300

    feat:Connecting gateway with orders microservice

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('Orders-Main');
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.PORT,
+      },
+    },
+  );
+
+  app.listen();
+  logger.log(`Microservice running on port ${envs.PORT}`);
 }
 bootstrap();
