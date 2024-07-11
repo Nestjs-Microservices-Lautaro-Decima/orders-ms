07a7368fecbd4038b2fdda34008631777013713a
Lautaro Decima <decimalautaro@gmail.com>
2024-07-10 22:18:33 -0300

    feat: installing dependences

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
@@ -14,6 +14,13 @@ async function bootstrap() {
         port: envs.PORT,
       },
     },
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
   );
 
   app.listen();
